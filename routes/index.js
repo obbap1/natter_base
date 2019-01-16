672cf488f6c724ed2d6955d3d89702b94c9edea2
obbap1 <paschal.obba@stu.cu.edu.ng>
2019-01-16 16:34:15 +0100

    Fix

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const controller = require('../controllers/auth-controller')
+const middleware = require('../middlewares/auth-middleware')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.post('/login',(req,res,))
+router.post('/login', controller.verifyLogin);
+router.get('/countries',middleware.validateToken,controller.getCountries);
+router.put('/countries',middleware.validateToken,controller.putCountries);
+router.delete('/countries',middleware.validateToken,controller.deleteCountries);
 
 module.exports = router;
